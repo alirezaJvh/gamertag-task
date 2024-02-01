d730ecff355a2bd96fe4a8eb99f086493544ce7c
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-01 17:57:11 +0100

    feat: styling the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,39 @@ export default async function Home() {
     <>
       <TheHeader />
       <div className="py-10 flex flex-col items-center w-full min-h-[calc(100vh-50px)] h-full">
-        <form action={createTodo}>
-          <input name="name" placeholder="Add a todo" />
-          <button type="submit">Add</button>
+        <div className="text-xl font-semibold pb-5">Todo list app</div>
+        <form className="w-[400px] flex justify-between" action={createTodo}>
+          <input
+            className="border border-gray-300 px-2 outline-none"
+            name="name"
+            placeholder="Add a todo"
+          />
+          <button
+            className="rounded-md bg-blue-400 px-4 py-2 text-white"
+            type="submit"
+          >
+            Add
+          </button>
         </form>
 
-        {(!todos || todos.length === 0 || errors) && (
-          <div>
-            <p>No todos, please add one.</p>
-          </div>
-        )}
-
-        <ul>
-          {todos.map(todo => {
-            return <li style={{ listStyle: "none" }}>{todo.name}</li>;
-          })}
-        </ul>
+        <div className="w-[400px] p-3 mt-4 rounded-md border border-gray-300">
+          {(!todos || todos.length === 0 || errors) && (
+            <div>
+              <p>No todos, please add one.</p>
+            </div>
+          )}
+          <ul>
+            {todos.map((todo, index) => {
+              return (
+                <li
+                  className={`list-none py-2 ${index !== todos.length - 1 ? "border-b border-gray-300" : ""}`}
+                >
+                  {todo.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
       <TheFooter />
     </>
