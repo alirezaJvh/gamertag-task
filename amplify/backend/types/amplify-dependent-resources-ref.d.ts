771ec41abfabe9f0dd027706d15cdc2861fb182a
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-01 14:45:18 +0100

    feat: create backend with amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+  api: {
+    gamerttagtask: {
+      GraphQLAPIEndpointOutput: "string";
+      GraphQLAPIIdOutput: "string";
+      GraphQLAPIKeyOutput: "string";
+    };
+  };
+};
