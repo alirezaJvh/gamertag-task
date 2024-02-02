17885e5ac2a32057bd6d5e08d12faf0cfd11dad1
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-02 09:02:58 +0100

    feat: use graphqlCall funtion in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { createTodo, getTodo } from "@/lib/graphqlCall";
+import { graphqlCall } from "@/lib/graphqlCall";
 import { TheFooter } from "@/components/TheFooter";
 import { TheHeader } from "@/components/TheHeader";
+import { revalidatePath } from "next/cache";
 
 export default async function Home() {
-  const { data, errors } = await getTodo();
+  const { data, errors } = await graphqlCall("listTodos");
 
-  const todos = data.listTodos.items;
+  const createTodo = async (formData: FormData) => {
+    "use server";
+
+    await graphqlCall("createTodo", {
+      input: { name: formData.get("name")?.toString() ?? "" },
+    });
+    revalidatePath("/");
+  };
+
+  const todos = data?.listTodos?.items;
   return (
     <>
       <TheHeader />
@@ -26,22 +36,22 @@ export default async function Home() {
         </form>
 
         <div className="w-[400px] p-3 mt-4 rounded-md border border-gray-300">
-          {(!todos || todos.length === 0 || errors) && (
+          {!todos || todos.length === 0 || errors ? (
             <div>
               <p>No todos, please add one.</p>
             </div>
-          )}
-          <ul>
-            {todos.map((todo, index) => {
+          ) : (
+            todos.map((todo, index) => {
               return (
                 <li
                   className={`list-none py-2 ${index !== todos.length - 1 ? "border-b border-gray-300" : ""}`}
                 >
-                  {todo.name}
+                  {todo?.name}
                 </li>
               );
-            })}
-          </ul>
+            })
+          )}
+          <ul>{}</ul>
         </div>
       </div>
       <TheFooter />
